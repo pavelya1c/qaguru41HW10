94d95648b101f3ce9fb837c7ee206d431711cf8b
Pavel Ya <pavelya1c@gmail.com>
2026-05-19 12:25:44 +0400

    negativeTestsEdit

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -7,52 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class NegativeTests {
 
     @Test
-    public void testPositive1() {
+    public void NegativeTests1() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive2() {
+    public void NegativeTests2() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive3() {
+    public void NegativeTests3() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive4() {
+    public void NegativeTests4() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive5() {
+    public void NegativeTests5() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive6() {
+    public void NegativeTests6() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive7() {
+    public void NegativeTests7() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive8() {
+    public void NegativeTests8() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive9() {
+    public void NegativeTests9() {
         assertTrue(false);
     }
 
     @Test
-    public void testPositive10() {
+    public void NegativeTests10() {
         assertTrue(false);
     }
 
